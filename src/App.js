df8be578599eb2ca4cb24714f0a21d96b0962587
An Glebski <gluhova19962010@mail.ru>
2021-08-10 09:45:05 +0300

    add post sorting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,6 +10,7 @@ function App() {
     {id: 2, title: 'Python', body: 'Description'},
     {id: 3, title: 'C#', body: 'Description'}
   ]);
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,9 +20,26 @@ function App() {
     setPosts(posts.filter(item => item.id !== post.id))
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0'}} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
       {posts.length !== 0
         ?
         <PostList remove={removePost} posts={posts} title="Список постов 1" />
