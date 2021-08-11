208f2df2f3d24c2c0d627b2d752af41618dcb2d5
An Glebski <gluhova19962010@mail.ru>
2021-08-11 20:07:02 +0300

    add ui/loader

--- src/App.js
+++ src/App.js
@@ -7,22 +7,17 @@ import PostService from './API/PostService';
 import {usePosts} from './hooks/usePosts';
 import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
-
+import MyLoader from './components/UI/loader/MyLoader';
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'Python', body: 'Description'},
-    {id: 3, title: 'C#', body: 'Description'}
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({
     sort: '',
     query: ''
   });
-
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -34,8 +29,10 @@ function App() {
   };
 
   async function fetchPosts() {
+    setIsPostsLoading(true);
     const posts = await PostService.getAll();
-    setPosts(posts)
+    setPosts(posts);
+    setIsPostsLoading(false);
   };
 
   useEffect(() => {
@@ -57,10 +54,15 @@ function App() {
       <PostFilter
         filter={filter}
         setFilter={setFilter}/>
-      <PostList
+      { isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+            <MyLoader />
+          </div>
+        : <PostList
         remove={removePost}
         posts={sortedAndSearchedPosts}
         title="Список постов 1" />
+      }
     </div>
   );
 }
