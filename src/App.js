1087f8397aaca7a77324ae38414fd3ff279e9194
An Glebski <gluhova19962010@mail.ru>
2021-08-17 14:45:54 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,88 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import { BrowserRouter} from 'react-router-dom';
 import './styles/App.css';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import PostFilter from './components/PostFilter';
-import PostService from './API/PostService';
-import {usePosts} from './hooks/usePosts';
-import {useFetching} from './hooks/useFetching';
-import {getPageCount} from './utils/pages';
-import MyModal from './components/UI/modal/MyModal';
-import MyButton from './components/UI/button/MyButton';
-import MyLoader from './components/UI/loader/MyLoader';
-import Pagination from './components/UI/pagination/Pagination';
+import Navbar from './components/UI/navbar/Navbar';
+import AppRouter from './components/AppRouter';
 
 function App() {
-
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({
-    sort: '',
-    query: ''
-  });
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching( async(limit, page) => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers['x-total-count'];
-    setTotalPages(getPageCount(totalCount, limit));
-  });
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter(item => item.id !== post.id))
-  };
-
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
-
-  useEffect(() => {
-    fetchPosts(limit, page)
-  }, []);
-
-  return (
-    <div className="App">
-      <MyButton
-        style={{marginTop: 30}}
-        onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: '15px 0'}} />
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}/>
-      { postError &&
-        <h3>Произошла ошибка ${postError}</h3>
-      }
-      { isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-            <MyLoader />
-          </div>
-        : <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Список постов" />
-      }
-      <Pagination
-        page={page}
-        changePage={changePage}
-        totalPages={totalPages} />
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Posts from '../pages/Posts';
+import Error from '../pages/Error';
+
+const AppRouter = () => {
+  return (
+    <Switch>
+      <Route path="/posts">
+        <Posts />
+      </Route>
+      <Route path="/error">
+        <Error />
+      </Route>
+      <Redirect to="/error" />
+    </Switch>
+  );
+};
+
+export default AppRouter;
