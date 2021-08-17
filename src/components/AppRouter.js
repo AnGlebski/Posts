f5c2175a7202a59738338839283a3ae9cdded780
An Glebski <gluhova19962010@mail.ru>
2021-08-17 17:59:06 +0300

    add dynamic navigation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import Posts from '../pages/Posts';
+import PostIdPage from '../pages/PostIdPage';
 import Error from '../pages/Error';
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Route path="/posts">
+      <Route exact path="/posts">
         <Posts />
+      </Route>
+      <Route exact path="/posts/:id">
+        <PostIdPage />
       </Route>
       <Route path="/error">
         <Error />
